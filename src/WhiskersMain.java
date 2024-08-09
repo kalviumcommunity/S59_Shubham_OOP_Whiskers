4f6d96833512b77637e270271531f3a239bbff4a
ShubhamThakur025 <thakur0805@gmail.com>
2024-08-09 14:57:26 +0530

    Added array of objects for pet storage

--- src/WhiskersMain.java
+++ src/WhiskersMain.java
@@ -1,8 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class WhiskersMain{
     public static void main(String[] args){
-        Pet pet1 = new Pet("Ginger", 5, "adopted");
-        pet1.displayInfo();
+        List <Pet> petStorage = new ArrayList<>();
+        petStorage.add(new Pet("Ginger", 5, "AVAILABLE"));
+        petStorage.add(new Pet("Tommy", 8, "PENDING"));
+        petStorage.add(new Pet("Ginger", 5, "UNAVAILABLE"));
 
+        for(Pet pet : petStorage){
+            pet.displayInfo();
+        }
         System.out.println("===========================");
 
         User user1 = new User("Ram", "admin");
